a6963e8163da006c0a6dd7b12f7068935d3b7173
Sara Sanchez <ssancam391@g.educaand.es>
2023-11-05 21:33:01 +0100

    save

--- src/P2_2_22_paresImparesDig.py
+++ src/P2_2_22_paresImparesDig.py
@@ -1,39 +1,21 @@
 """Crear un programa que solicite el ingreso de números enteros positivos, hasta que el usuario ingrese el 0. 
 Por cada número, informar cuántos dígitos pares y cuántos impares tiene. 
 Al finalizar, informar la cantidad de dígitos pares y de dígitos impares leídos en total."""
-
-def num_pares_impares ():
+lista_numeros = []
+cadena_numeros = ""
+suma_pares = 0
+suma_impares = 0
+    
+seguir = True
+while seguir:
         
-    lista_numeros = []
-    suma_pares = 0
-    suma_impares = 0
-    
-    seguir = True
-    
-    while seguir:
-        print("Ingresa un numero entero: ")
-        numero = str(input())
+    numero = int(input("Ingresa un numero entero: "))
+        
+    if (numero == 0) or seguir == False:
+        seguir = False
+            
+    elif (numero > 0):
         
         lista_numeros.append(numero)
         
-        if (numero == 0):
-            seguir = False 
-            
-    
-    for cifra in lista_numeros:
-        for digito in cifra:
-            
-            if (digito%2 == 0):
-               print(f"De {cifra}: {digito} es par.")
-               suma_pares += 1
-            else:
-               print(f"De {cifra}: {digito} es impar.")
-               suma_impares += 1
-                
-   
-def main(): 
-    
-    num_pares_impares()
-
-if __name__=="__main__":
-    main()
+print (lista_numeros)
